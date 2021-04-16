d0273ce7fb038bf2f41a3c5040dc038b107ca11d
afsa yassine <afsayassine@gmail.com>
2021-04-16 23:00:03 +0100

    gfdf

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,22 +3,27 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\DProtusers;
+use App\Repository\DProtusersmobileRepository;
+use App\Repository\DProtusersRepository;
 use Symfony\Component\HttpFoundation\Response;
 class SecurityController extends AbstractController
 {
  /**
   * @Route("/login",name="app_login",methods={"POST"})
   */
- public function Login()
+ public function Login(DProtusersmobileRepository $rep)
  {
     if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
         return $this->json([
             'error' => 'Invalid login request: check that the Content-Type header is "application/json".',
            ], 400);
  } else { $name=$this->getUser()->getUsername();
- 
+          $prot=$rep->findOneBy(['protmUser' => $name]);
+          $email=$prot->getProtmEmail(); 
         return $this->json(["content"=>'bienvenue '.$name,
         "result"=>"true",
+        "name"=>"$name",
+        "email"=>"$email",
         'status'=>'1',
         'status_text'=>'login success'],200);
  }}
